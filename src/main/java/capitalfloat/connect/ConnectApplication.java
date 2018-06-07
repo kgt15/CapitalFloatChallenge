13c19a43873bf0d038a2531e99e7d60a618786a1
kgt15 <k.georgethomas15@gmail.com>
2018-06-07 11:02:09 +0530

    Basic Spring Boot

---
+++ src/main/java/capitalfloat/connect/ConnectApplication.java
@@ -0,0 +1,12 @@
+package capitalfloat.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+}
